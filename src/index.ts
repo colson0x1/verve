717d3df0ebe8b6e67475a93936d207e598317ba7
Colson <colsoncalifornication@gmail.com>
2023-08-18 18:59:19 +0545

    implement set to update user properties

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,7 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Colson', age: 24 });
 
+user.set({ name: 'Colson NYC' });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
