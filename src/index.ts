142a40dda0f59246e55b158796203c96a0a438eb
Colson <colsoncalifornication@gmail.com>
2023-08-18 19:57:22 +0545

    trigger event callbacks

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,14 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Colson', age: 24 });
 
-user.on('change', () => {});
-user.on('dope', () => {});
-user.on('awsm', () => {});
+user.on('change', () => {
+  console.log('change event was triggered');
+});
+user.on('dope', () => {
+  console.log('dope event was triggered');
+});
+user.on('awsm', () => {
+  console.log('awsm event was triggered');
+});
 
-console.log(user);
+user.trigger('awsm');
