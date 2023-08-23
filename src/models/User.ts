023cc996c137baf27a18200b9f11780d33458850
Colson <colsoncalifornication@gmail.com>
2023-08-23 12:28:59 +0545

    extract fetch and save to separate class sync

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,3 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
 interface UserProps {
@@ -20,21 +19,5 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
+  
 }
