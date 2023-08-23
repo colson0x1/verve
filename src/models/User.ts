abfe1a591131210229652441097b59edef3591d8
Colson <colsoncalifornication@gmail.com>
2023-08-23 20:31:36 +0545

    extract an attribute class

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,5 @@
 import { Eventing } from './Eventing';
-import { Sync } from './Sync'
+import { Sync } from './Sync';
 
 export interface UserProps {
   id?: number;
@@ -12,16 +12,4 @@ const rootUrl = 'http://localhost:3000/users';
 export class User {
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
-
-  constructor(private data: UserProps) {}
-
-  get(propName: string): number | string {
-    return this.data[propName];
-  }
-
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-
-  
 }
