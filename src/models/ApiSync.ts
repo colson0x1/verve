bc3fdf2e5fac927d691f66e30271d90444642c61
Colson <colsoncalifornication@gmail.com>
2023-08-24 11:03:06 +0545

    final user refractor

--- src/models/ApiSync.ts
+++ src/models/ApiSync.ts
@@ -3,7 +3,7 @@ import axios, { AxiosPromise } from 'axios';
 interface HasId {
   id?: number;
 }
-export class Sync<T extends HasId> {
+export class ApiSync<T extends HasId> {
   constructor(public rootUrl: string) {}
 
   fetch(id: number): AxiosPromise {
