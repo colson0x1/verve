0a53b307aac7eeec411ae4bcc5c0e85cc4570a39
Colson <colsoncalifornication@gmail.com>
2023-08-24 17:17:53 +0545

    bind event handlers

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -4,8 +4,14 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:button': this.onButtonClick,
+      'mouseenter:h1': this.onHeaderHover,
     };
   }
+
+  onHeaderHover(): void {
+    console.log('H1 was hovered over');
+  }
+
   onButtonClick(): void {
     console.log('Hello from VERVE');
   }
@@ -15,14 +21,28 @@ export class UserForm {
       <div>
         <h1>User Form</h1>
         <input />
-        <button>Do something</button>
+        <button>Console Log</button>
       </div>
     `;
+  }
+
+  bindEvents(fragment: DocumentFragment): void {
+    const eventsMap = this.eventsMap();
+
+    for (let eventKey in eventsMap) {
+      const [eventName, selector] = eventKey.split(':');
+
+      fragment.querySelectorAll(selector).forEach((element) => {
+        element.addEventListener(eventName, eventsMap[eventKey]);
+      });
+    }
   }
 
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
+
+    this.bindEvents(templateElement.content);
 
     this.parent.append(templateElement.content);
   }
