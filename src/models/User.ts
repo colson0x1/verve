c939ff7740d88aeba909c72813e7dfd0c6f306c0
Colson <colsoncalifornication@gmail.com>
2023-08-24 13:42:25 +0545

    add static class method for collections

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,7 +2,7 @@ import { Model } from './Model';
 import { Attributes } from './Attributes';
 import { ApiSync } from './ApiSync';
 import { Eventing } from './Eventing';
-
+import { Collection } from './Collection';
 export interface UserProps {
   id?: number;
   name?: string;
@@ -17,6 +17,12 @@ export class User extends Model<UserProps> {
       new Attributes<UserProps>(attrs),
       new Eventing(),
       new ApiSync<UserProps>(rootUrl)
+    );
+  }
+
+  static buildUserCollection(): Collection<User, UserProps> {
+    return new Collection<User, UserProps>(rootUrl, (json: UserProps) =>
+      User.buildUser(json)
     );
   }
 }
