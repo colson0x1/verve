4d5882af547421531f2561ac80b9e267c3a2748a
Colson <colsoncalifornication@gmail.com>
2023-08-24 16:44:13 +0545

    implement events map

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,11 +1,21 @@
 export class UserForm {
   constructor(public parent: Element) {}
 
+  eventsMap(): { [key: string]: () => void } {
+    return {
+      'click:button': this.onButtonClick,
+    };
+  }
+  onButtonClick(): void {
+    console.log('Hello from VERVE');
+  }
+
   template(): string {
     return `
       <div>
         <h1>User Form</h1>
         <input />
+        <button>Do something</button>
       </div>
     `;
   }
