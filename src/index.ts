c70e027a30918e339ac58c61d898604e93b1b5c7
Colson <colsoncalifornication@gmail.com>
2023-08-24 09:03:57 +0545

    resolve context issues

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Colson Downtown LA', age: 33 });
 
+console.log(user.get('name'));
+
 user.on('change', () => {
-  console.log('changed yay');
+  console.log('User was changed');
 });
+
+user.trigger('change');
--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,7 +1,7 @@
 export class Attributes<T extends object> {
   constructor(private data: T) {}
 
-  get<K extends keyof T>(key: K): T[K] {
+  get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
   }
 
