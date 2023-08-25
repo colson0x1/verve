fe9ccbcc72388564b011c62ee164d2ee0fe658ee
Colson <colsoncalifornication@gmail.com>
2023-08-25 09:21:35 +0545

    add model properties

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
-  const userForm = new UserForm(rootElement);
+  const user = User.buildUser({ name: 'Airlines', age: 45 });
+  const userForm = new UserForm(rootElement, user);
   userForm.render();
 } else {
   console.error(`Element with id 'root' not found in the DOM.`);
