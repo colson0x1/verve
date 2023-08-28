0590121a3d3101d7e13f4f86c74905046bc2b76d
Colson <colsoncalifornication@gmail.com>
2023-08-28 14:56:57 +0545

    implement change name

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -14,8 +14,17 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+
+    const name = input.value;
+
+    this.model.set({ name });
+  };
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
@@ -28,7 +37,7 @@ export class UserForm {
         <div>User name: ${this.model.get('name')}</div>
         <div>User age: ${this.model.get('age')}</div>
         <input />
-        <button>Magic Button</button>
+        <button class='set-name'>Change Name</button>
         <button class='set-age'>Set Random Age</button>
       </div>
     `;
