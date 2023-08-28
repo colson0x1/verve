34a1bc79679675ca77bf80a874b4f830e34a4be7
Colson <colsoncalifornication@gmail.com>
2023-08-28 20:03:20 +0545

    implement collection view

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
+
+const users = new Collection(
+  'http://localhost:3000/users',
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on('change', () => {
+  const root = document.getElementById('root');
+
+  if (root) {
+    new UserList(root, users).render();
+  }
+});
+
+users.fetch();
+
+
+/*
+@ update data
+
 import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const root = document.getElementById('root');
 
-const user = User.buildUser({ name: 'Airlines', age: 45 });
+const user = User.buildUser({ name: 'American Airlines', age: 45 });
 
 if (root) {
   const userEdit = new UserEdit(root, user);
@@ -14,3 +39,4 @@ if (root) {
 } else {
   throw new Error(`Root element not found`);
 }
+*/
