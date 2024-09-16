fa17fa5b23b7656b4a4abac93e091416a178ae39
Colson <colsoncalifornication@gmail.com>
2024-09-16 12:49:06 +0545

    central export models and views

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+/* Exporting models */
+export { User, UserProps } from './models/User';
+export { Collection } from './models/Collection';
+export { ApiSync } from './models/ApiSync';
+export { Attributes } from './models/Attributes';
+export { Model } from './models/Model';
+export { Eventing } from './models/Eventing';
+
+/* Exporting views */
+export { UserEdit } from './views/UserEdit';
+export { UserForm } from './views/UserForm';
+export { UserList } from './views/UserList';
+export { UserShow } from './views/UserShow';
+export { CollectionView } from './views/CollectionView';
+export { View } from './views/View';
+
+/* Usage example to render a list of users in the root element */
 import { UserList } from './views/UserList';
 import { Collection } from './models/Collection';
 import { UserProps, User } from './models/User';
@@ -6,7 +23,7 @@ const users = new Collection(
   'http://localhost:3000/users',
   (json: UserProps) => {
     return User.buildUser(json);
-  }
+  },
 );
 
 users.on('change', () => {
@@ -18,7 +35,6 @@ users.on('change', () => {
 });
 
 users.fetch();
-
 
 /*
 @ update data
